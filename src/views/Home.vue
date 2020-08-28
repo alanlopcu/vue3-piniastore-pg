55f4f187d1cb61dfd35bd7dcc5775023a96d4b50
Alvaro Lopez <alanlopcu@gmail.com>
2020-08-28 17:18:21 +0200

    first commit

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,40 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <h3>Counter: {{counter}}</h3>
+    <br />
+    <button @click="increment">Increment</button>
+    <br />
+    <input v-model="val" type="number" />
+    <br />
+    <button @click="addValue(val)">Increment by value</button>
+    <br />
+    <button @click="reset">Reset</button>
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+import { defineComponent } from "vue";
+import { useCounterStore } from "@/store/counter";
 
 export default defineComponent({
-  name: 'Home',
-  components: {
-    HelloWorld,
+  name: "Home",
+  components: {},
+  data: () => ({
+    val: 0,
+  }),
+  setup() {
+    const counter = useCounterStore();
+    function reset() {
+      counter.patch({
+        counter: 0,
+      });
+    }
+    return {
+      reset,
+      increment: counter.incrementCounter,
+      counter: counter.getCounter,
+      addValue: counter.addValue,
+    };
   },
 });
 </script>
